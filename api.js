6e0dea14fcdc4162b74fc0ef11cea93085552174
Lauren Laskerr <llask@me.com>
2019-08-26 15:09:09 -0400

    using giffy api and bored api

--- api.js
+++ api.js
@@ -5,7 +5,14 @@
 
 document.querySelector("#no").onclick = () => {
   axios.get("https://www.boredapi.com/api/activity").then(data => {
-    console.log(data.data.activity);
+    console.log(data.data.activity)
+    
+    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=PAisOsDZTQhp368DqHfEh0KoWuAWZi7B&q=${data.data.activity}`).then(data => {
+      let url = data.data.data[0].images.original.url
+
+      document.body.style.backgroundImage =  `url(${url})`
+    })
+
     createResponseBox();
     document.querySelector("#dothis").innerHTML = `Okay, how about.....<br> ${data.data.activity}`
   })
@@ -17,6 +24,9 @@ document.querySelector("#no").onclick = () => {
 document.querySelector("#add").onclick = () => {
   event.preventDefault();
   let response = document.querySelector("#task").value
+    document.querySelector("#task").value = ""
+
+
   console.log(response)
   tasks.push(response);
 }
@@ -27,11 +37,24 @@ document.querySelector("#add").onclick = () => {
 document.querySelector("#yes").onclick = () => {
   let random = Math.floor(tasks.length * Math.random())
   let thing = tasks[random]
+
+
+  axios.get(`http://api.giphy.com/v1/gifs/search?api_key=PAisOsDZTQhp368DqHfEh0KoWuAWZi7B&q=${thing}`).then(data => {
+      let url = data.data.data[0].images.original.url
+
+      document.body.style.backgroundImage =  `url(${url})`
+    })
+
   createResponseBox();
   document.querySelector("#dothis").innerHTML = `Let's get this out of the way:<br> ${thing}`
 }
 
 
 function createResponseBox() {
-  document.querySelector("#dothis").setAttribute("style", "display: flex; justify-content: center; align-items: center; height: 100px; width: 500px; border: 2px solid black;")
+  document.querySelector("#dothis").setAttribute("style", "display: flex; justify-content: center; align-items: center; height: 200px; width: 600px;")
 }
+
+
+
+
+// PAisOsDZTQhp368DqHfEh0KoWuAWZi7B
