f595cb5e9fccec2b78734d60f7f561702596f4a3
Lauren Laskerr <llask@me.com>
2019-08-26 13:38:49 -0400

    avoid decision fatigue

--- api.js
+++ api.js
@@ -3,4 +3,35 @@
 // No => bored.api
 // Yes => randomly pick one.
 
+document.querySelector("#no").onclick = () => {
+  axios.get("https://www.boredapi.com/api/activity").then(data => {
+    console.log(data.data.activity);
+    createResponseBox();
+    document.querySelector("#dothis").innerHTML = `Okay, how about.....<br> ${data.data.activity}`
+  })
+}
 
+
+  let tasks = []
+
+document.querySelector("#add").onclick = () => {
+  event.preventDefault();
+  let response = document.querySelector("#task").value
+  console.log(response)
+  tasks.push(response);
+}
+
+
+
+
+document.querySelector("#yes").onclick = () => {
+  let random = Math.floor(tasks.length * Math.random())
+  let thing = tasks[random]
+  createResponseBox();
+  document.querySelector("#dothis").innerHTML = `Let's get this out of the way:<br> ${thing}`
+}
+
+
+function createResponseBox() {
+  document.querySelector("#dothis").setAttribute("style", "display: flex; justify-content: center; align-items: center; height: 100px; width: 500px; border: 2px solid black;")
+}
